b8792bae036dd99e97f4b472a7f67f98488f748d
megatron3000 <silvac1@student.lasalle.edu>
2014-09-02 11:28:25 -0400

    this is are my previous install steps for osticket

--- manifests/init.pp
+++ manifests/init.pp
@@ -39,3 +39,31 @@ class osticket {
 
 
 }
+
+
+#Lamp Stack Install Steps, 
+#2.	sudo apt-get install apache2
+#3.	sudo /etc/init.d/apache2 start
+#4.	sudo apt-get install php5 libapache2-mod-php5
+#5.	sudo /etc/init.d/apache2 restart
+#6.	touch /var/www/test.php
+#7.	http://10.21.7.100/test.php
+#8.	sudo apt-get install mysql-server
+#9.	mysql -u root –p
+#10.SET PASSWORD FOR root@localhost = Ubuntu
+#11.Sudo /etc/init.d/mysql start
+
+
+#Osticket Install steps,
+#12.	sudo mkdir /var/www/support.
+#13.	git clone https://github.com/osTicket/osTicket-1.8/archive/v1.8.5.tar.gz
+#14.	sudo tar xvzf osticket_v1.8.5.tar.gz     
+#15.	Moved all of the files from the /var/www/support/osticket_v1.8.5.tar.gz 
+#16.	sudo mv /var/www/osticket_v1.8.5.tar.gz /var/www/support/osticket_v1.8.5.tar.gz
+#17.	Renamed the file ost-config.sample.php 
+#18.	sudo mv /var/www/support/include/ost-config.sample.php /var/www/support/include/ost-config.php.
+#19.	sudo chmod 777 /var/www/support/include/ost-config.php.
+#20.	Pointed browser to https://10.21.7.100/support
+#21.	Answered installation questions
+#22.	Chmod 644 /var/www/support/incude/ost-conf.php
+#23.	Rm –rf /var/www/support/setup
